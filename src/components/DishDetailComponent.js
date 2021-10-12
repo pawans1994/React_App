3964268bc2f99f05d170b21dbb944c4d2ac3171e
pawans1994 <pawan.p.121@gmail.com>
2021-10-11 21:24:01 -0700

    Added Header and Footer

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,91 +1,84 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, List } from 'reactstrap';
 
-class DishDetail extends Component{
 
-    componentDidMount(){
-        console.log(this.props.selectedDish);
-    }
 
-    renderDish(dish){
-        if (dish != null){
-            return (
-                <Card>
-                    <CardImg width = "100%" src = {dish.image} alt = {dish.name}/>
-                    <CardBody>
-                        <CardTitle tag = "h5">{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        else{
-            return (
-                <div></div>
-            )
-        }
-    }
-
-    renderComments(dishComments){
-
-        if (!dishComments || !dishComments.length){
-            return(
-                <div></div>
-            )
-        }
-
-        else{
-            
-            const commentsView = dishComments.map((comm)=>{
-            
-                return(
-                    <div key = {comm.id}>
-                        <List type ="unstyled">
-                            <li>{comm.comment}</li>
-                            <br/>
-                            <li>--{comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}</li>
-                        </List>
-                    </div>
-                );
-            });
-            return(
-            <div>
-                <h4>Comments</h4>
-                {commentsView}
-            </div>
-            )
-        }
-
-        
-    }
-
-    render(){
-        
-        let comm;
-        const currentDish = this.props.selectedDish;
-        
-        if (currentDish != null){
-            comm = currentDish.comments;
-        return(
-            <div className = "container">
-            <div className = "row">
-                <div className = "col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.selectedDish)}
-                </div>
-                <div className = "col-12 col-md-5 m-1">
-                    {this.renderComments(comm)}
-                </div>
-            </div>
-            </div>
-            
-                    
+function RenderDish({dish}){
+    if (dish != null){
+        return (
+            <Card>
+                <CardImg width = "100%" src = {dish.image} alt = {dish.name}/>
+                <CardBody>
+                    <CardTitle tag = "h5">{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
         )
-        }
-        else{
+    }
+    else{
+        return (
+            <div></div>
+        )
+    }
+}
+
+function RenderComments({dishComments}){
+
+    if (!dishComments || !dishComments.length){
+        return(
+            <div></div>
+        )
+    }
+
+    else{
+        
+        const commentsView = dishComments.map((comm)=>{
+        
             return(
-                <div></div>
-            )
-        }
+                <div key = {comm.id}>
+                    <List type ="unstyled">
+                        <li>{comm.comment}</li>
+                        <br/>
+                        <li>--{comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}</li>
+                    </List>
+                </div>
+            );
+        });
+        return(
+        <div>
+            <h4>Comments</h4>
+            {commentsView}
+        </div>
+        )
+    }
+
+    
+}
+
+        
+const DishDetail = (props) =>{
+    let comm;
+    const currentDish = props.selectedDish;
+    
+    if (currentDish != null){
+        comm = currentDish.comments;
+    return(
+        <div className = "container">
+        <div className = "row">
+            <div className = "col-12 col-md-5 m-1">
+                <RenderDish dish = {currentDish} />
+            </div>
+            <div className = "col-12 col-md-5 m-1">
+                <RenderComments dishComments = {comm} />
+            </div>
+        </div>
+        </div>           
+    )
+    }
+    else{
+        return(
+            <div></div>
+        )
     }
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent'
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import '../App.css';
 
 class Main extends Component {
@@ -22,14 +23,11 @@ class Main extends Component {
   render() {
   return (
     <div>
-      <Navbar dark color = "primary">
-        <div className="container">
-        <NavbarBrand href="/">Ristorante Con Confusion</NavbarBrand>
-        </div>
-      </Navbar>
+        <Header/>
       <Menu dishes = {this.state.dishes}
             onClick = {(dishId)=>this.onDishSelect(dishId)}/>
       <DishDetail selectedDish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+      <Footer/>
     </div>
   );
 }
